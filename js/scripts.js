4c672314b19b6ba34d4728ecd5b0cf97f729fa52
Darwin Bracamonte <bracamontedar@gmail.com>
2018-04-08 01:07:09 -0400

    Feat (Answers): Implementado los tabs para las preguntas-respuestas y plugin de Hightligth para mostar el codigo.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
+// Lightbox
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
   event.preventDefault();
   $(this).ekkoLightbox();
+});
+
+// Highlight
+$(document).ready(function() {
+  $('pre code').each(function(i, block) {
+    hljs.highlightBlock(block);
+  });
 });
