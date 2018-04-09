a71ccfcbde4e938c092544278064f8e8f12fd830
Darwin Bracamonte <bracamontedar@gmail.com>
2018-04-08 20:31:14 -0400

    Feat (Login): Implementado login con firebase.

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,68 @@ $(document).ready(function() {
     hljs.highlightBlock(block);
   });
 });
+
+(function() {
+  // Initialize Firebase
+  const config = {
+    apiKey: "....",                                 // Auth / General Use
+    authDomain: "YOUR_APP.firebaseapp.com",         // Auth with popup/redirect
+    databaseURL: "https://YOUR_APP.firebaseio.com", // Realtime Database
+    storageBucket: "YOUR_APP.appspot.com",          // Storage
+    messagingSenderId: "123456789"                  // Cloud Messaging
+  };
+  firebase.initializeApp(config);
+  
+  // Login form elements
+  const txtEmail = document.getElementById('inputEmail');
+  const txtPassword = document.getElementById('inputPassword');
+  const btnLogin = document.getElementById('btnLogin');
+  const btnLogout = document.getElementById('btnLogout');
+  const btnSignUp = document.getElementById('btnSignUp');
+  const btnNavLogin = document.getElementById('btnNavLogin');
+
+  btnLogin.addEventListener('click', e => {
+    // Get email and password
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // Sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise
+      .then(e => console.log('Login:', e))
+      .catch(e => console.log('Login Error:', e.message))
+  })
+
+  btnSignUp.addEventListener('click', e => {
+    // Get email and password
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // Sign in
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise
+      .then(e => console.log('SignUp:', e))
+      .catch(e => console.log('SignUp Error:', e.message))
+  })
+
+  btnLogout.addEventListener('click', e => {
+    firebase.auth().signOut();
+  })
+
+  // RealTime listener
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      console.log(`User: ${user}`);
+      btnNavLogin.classList.add('d-none');
+      btnLogout.classList.remove('d-none');
+      // User is signed in.
+      const displayName = user.displayName;
+      const photoURL = user.photoURL;
+    } else {
+      console.log('Not logged in');
+      btnNavLogin.classList.remove('d-none');
+      btnLogout.classList.add('d-none');
+    }
+  });
+
+}());
